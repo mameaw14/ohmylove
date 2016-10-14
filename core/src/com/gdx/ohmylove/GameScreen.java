09d865910284e904f4d8aa09fd5fb6283701a150
Mameaw14 <pinyapornster@gmail.com>
2016-10-14 13:33:44 +0700

    gamescreen

---
+++ core/src/com/gdx/ohmylove/GameScreen.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gdx.ohmylove;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ *
+ * @author nimo
+ */
+public class GameScreen extends ScreenAdapter{
+    OhmyloveGame ohmyloveGame;
+    public GameScreen(OhmyloveGame ohmylovegame) {
+	this.ohmyloveGame = ohmyloveGame;
+    }
+    
+    @Override
+    public void render(float delta) {
+	Gdx.gl.glClearColor(37, 40, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+}
---
+++ desktop/src/com/gdx/ohmylove/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.ohmylove.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.ohmylove.OhmyloveGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OhmyloveGame(), config);
+	}
+}
